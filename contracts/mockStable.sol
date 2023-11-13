0029ff463d8fd347416e3bbfb47992daa5144be6
Jovells <Jovells.appiah@gmail.com>
2023-11-13 18:14:38 +0000

    write mockusdc contract

---
+++ contracts/mockStable.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+//test usdt contract for hardhat tests
+//use openzepplin
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+//test usdt contract for hardhat tests
+//implement ERC20 contract
+contract Stablecoin is ERC20 {
+    function decimals() public override pure returns (uint8){
+        return 6;
+    }
+    //constructor
+    constructor() ERC20("MockUSDC", "MUSDC") {
+        //mint 10,000 tokens to the deployer
+        _mint(msg.sender, 100000000000 * 10**decimals());
+    }
+    function mint() public{
+        _mint(msg.sender, 10 * 10**decimals());
+    }
+}
